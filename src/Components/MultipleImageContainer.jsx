b3f3c7a2bb89ef91a47014f629df7e63be590a14
love-to-code108 <love.to.code108@gmail.com>
2026-04-16 19:47:25 +0530

    starting working on the workshop page

--- src/Components/MultipleImageContainer.jsx
+++ src/Components/MultipleImageContainer.jsx
@@ -6,16 +6,16 @@ const MultipleImageContainer = ({children , className}) => {
     return(
         <div className={`relative p-4 ${className}`}>
 
-            <div className=" bg-white10 w-[calc(100%+30px)] h-[1px]
+            <div className=" bg-white20 w-[calc(100%+30px)] h-[1px]
              absolute bottom-[-1px] left-[-15px]
             "/>
-            <div className=" bg-white10 w-[calc(100%+30px)] h-[1px]
+            <div className=" bg-white20 w-[calc(100%+30px)] h-[1px]
              absolute top-[-1px] left-[-15px]
             "/>
-            <div className=" bg-white10 h-[calc(100%+30px)] w-[1px]
+            <div className=" bg-white20 h-[calc(100%+30px)] w-[1px]
              absolute left-[-1px] top-[-15px]
             "/>
-            <div className=" bg-white10 h-[calc(100%+30px)] w-[1px]
+            <div className=" bg-white20 h-[calc(100%+30px)] w-[1px]
              absolute right-[-1px] top-[-15px]
             "/>
             {children}
