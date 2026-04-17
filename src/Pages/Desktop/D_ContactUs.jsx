3bb1262fe084ccf382457a1799fe7b40fc5eb0e8
love-to-code108 <love.to.code108@gmail.com>
2026-04-17 16:27:34 +0530

    i am about to go full into form so i am saving here in case everything goes south this should help me get things back
    i am fucking exited

--- src/Pages/Desktop/D_ContactUs.jsx
+++ src/Pages/Desktop/D_ContactUs.jsx
@@ -1,13 +1,89 @@
+import D_ContactUsFooter from "@/Components/D_ContactUsFooter";
+import DesktopSecondaryHeroSections from "@/Components/DesktopSecondaryHeroSections";
+import Heading from "@/Components/Heading";
+import SecondaryPageHeroHeading from "@/Components/SecondaryPageHeroHeading";
+import Text from "@/Components/Text";
 import { motion } from "motion/react";
 
 const D_ContactUs = () => {
 
 
 
-    return(
+    return (
         <motion.div className=" switchDesktop">
 
-            this is the contact us page
+            {/* the big container */}
+            <div className="w-full flex flex-col items-center">
+
+
+
+
+                {/* the container */}
+                <div className=" flex flex-col items-center
+                
+                
+                w-[76rem]">
+
+
+
+
+                    {/* the hero section */}
+                    <DesktopSecondaryHeroSections
+                        bulletpointText={"CONTACT V1.0"}
+                        heading={"Contact Us"}
+                        subText={"Connect with us to discuss your requirements, scheduling, or any queries."}
+                        paragraphText={"Dedicated channel for coordinating workshops, discussing requirements, and planning sessions with institutions."}
+                    />
+
+
+
+
+
+
+                    {/* the container for the form and the left text */}
+                    <div className="w-full flex justify-between mb-[200px]">
+
+
+
+
+                        {/* the text */}
+                        <div>
+                            <SecondaryPageHeroHeading
+                                heading={"NAADH Innovation"}
+                                subText={"National Aerospace Agriculture Defence & Hospitality"}
+                                className={"mb-[30px]"}
+                            />
+
+
+                            <Text>
+                                Contact Numbers :
+                                <br /><br />
+                                +91 86373 82771 <br />
+                                +91 80015 03977
+                                <br /><br />
+                                contact@naadhinnovation.com
+                            </Text>
+                        </div>
+
+
+
+
+
+                        {/* the form */}
+                        
+
+
+
+
+                    </div>
+                        {/* the footer */}
+                        <D_ContactUsFooter/>
+
+
+
+
+                </div>
+            </div>
         </motion.div>
     );
 }
